a571b0b58a8aa9cb9cecae7025d0d10eaeac17b5
Otaxonov <Otaxonov_98@mail.ru>
2025-03-27 23:32:28 +0500

    Django Channels Redis

--- DCH/asgi.py
+++ DCH/asgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from Test.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DCH.settings')
 
@@ -18,5 +19,6 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": URLRouter(websocket_urlpatterns)
     # Just HTTP for now. (We can add other protocols later.)
 })
