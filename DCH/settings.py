993907203d1109e029545e9e1488c60e11c4ea81
Otaxonov <Otaxonov_98@mail.ru>
2025-03-27 23:20:33 +0500

    Django Channels Redis

--- DCH/settings.py
+++ DCH/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,6 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'DCH.wsgi.application'
+ASGI_APPLICATION = 'DCH.asgi.application'
 
 
 # Database
