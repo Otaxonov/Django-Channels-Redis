505f313444217e309b73de8bd8f6b16074e3690d
Otaxonov <Otaxonov_98@mail.ru>
2025-03-27 23:30:35 +0500

    Django Channels Redis

---
+++ Test/consumers.py
@@ -0,0 +1,23 @@
+from channels.consumer import SyncConsumer
+from channels.exceptions import StopConsumer
+from time import sleep
+
+
+class MySyncConsumer(SyncConsumer):
+    def websocket_connect(self, event):
+        self.send({
+            "type": "websocket.accept",
+        })
+
+    def websocket_receive(self, event):
+        print(f'Message From Client: {event['text']}')
+
+        for i in range(10):
+            self.send({
+                "type": "websocket.send",
+                "text": str(i),
+            })
+            sleep(1)
+
+    def websocket_disconnect(self, event):
+        raise StopConsumer()
